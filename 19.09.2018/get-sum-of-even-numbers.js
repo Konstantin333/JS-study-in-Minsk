1495b9c017692410e3e5f18e43abb8b874afd7a0
Konstantin333 <kp.dan95@gmail.com>
2018-09-19 19:51:12 +0300

    РЕШИЛ ЗАДАЧУ: Вернуть суммарную длину строк с нечётной длиной

--- 19.09.2018/get-sum-of-even-numbers.js
+++ 19.09.2018/get-sum-of-even-numbers.js
@@ -1,14 +1,14 @@
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function getSum(a) {
+function getSumOfEvenNumbers(a) {
     var sum = 0;
     for (i = 0; i < a.length; i++) {
-        if (a[i] % 2 != 0) {
-            sum += i;
+        if (a[i] % 2 == 0) {
+            sum += a[i];
         }
     }
     return sum;
 }
 
-var result = getSum(array);
+var result = getSumOfEvenNumbers(array);
 console.log(result);
---
+++ 19.09.2018/get-sum-of-odd-line-lengths.js
@@ -0,0 +1,15 @@
+var array = ['Max', 124, 'Konstantin', 13357, 'Alex', 23, 'Anastasia'];
+
+function getSumOfOddLengths(a) {
+    var sum = 0;
+    for (i = 0; i < a.length; i++) {
+        if (a[i].length % 2 != 0) {
+            var str = String(a[i]).length
+            sum += str;
+        }
+    }
+    return sum;
+}
+
+var result = getSumOfOddLengths(array);
+console.log(result);
