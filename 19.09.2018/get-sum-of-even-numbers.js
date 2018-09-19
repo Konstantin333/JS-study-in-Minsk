b5e11579e706d26fe28dbea903bb77ce068e1127
Konstantin333 <kp.dan95@gmail.com>
2018-09-19 20:27:13 +0300

    Исправил все замечания

--- 19.09.2018/get-sum-of-even-numbers.js
+++ 19.09.2018/get-sum-of-even-numbers.js
@@ -3,7 +3,7 @@ var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function getSumOfEvenNumbers(a) {
     var sum = 0;
     for (i = 0; i < a.length; i++) {
-        if (a[i] % 2 == 0) {
+        if (a[i] % 2 === 0) {
             sum += a[i];
         }
     }
--- 19.09.2018/get-sum-of-odd-line-lengths.js
+++ 19.09.2018/get-sum-of-odd-line-lengths.js
@@ -3,8 +3,8 @@ var array = ['Max', 124, 'Konstantin', 13357, 'Alex', 23, 'Anastasia'];
 function getSumOfOddLengths(a) {
     var sum = 0;
     for (i = 0; i < a.length; i++) {
-        if (a[i].length % 2 != 0) {
-            var str = String(a[i]).length
+        if (a[i].length % 2 !== 0) {
+            var str = String(a[i]).length;
             sum += str;
         }
     }
