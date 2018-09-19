b17a0b065a235d836fdfe9d020cc14a22ffc8f8f
Konstantin333 <kp.dan95@gmail.com>
2018-09-19 22:28:03 +0300

    Выполнил 4-ю задачу

---
+++ 19.09.2018/get-array-of-multiple-four.js
@@ -0,0 +1,17 @@
+var array = [];
+for (var i = 1; i <= 100; i++) {
+    array.push(i)
+}
+
+function getArrayOfMultipleFour (a) {
+    var newArray = [];
+    for (i = 0; i < a.length; i++) {
+        if (a[i] % 4 === 0) {
+            newArray.push(a[i]);
+        }
+    }
+    return newArray;
+}
+
+var result = getArrayOfMultipleFour(array);
+console.log(result);
